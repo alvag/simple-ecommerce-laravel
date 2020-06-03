c2e149324097d99a65ed4d02f68f2cc9eb195caf
Max Alva <malva@peruapps.com.pe>
2020-06-02 23:16:54 -0500

    ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return 'Respuesta desde controlador';
+    }
+
+    public function create()
+    {
+        return 'Respuesta desde controlador';
+    }
+
+    public function store()
+    {
+        return 'Respuesta desde controlador';
+    }
+
+    public function show($product)
+    {
+        return 'Respuesta desde controlador';
+    }
+
+    public function edit($product)
+    {
+        return 'Respuesta desde controlador';
+    }
+
+    public function update($product)
+    {
+        return 'Respuesta desde controlador';
+    }
+
+    public function destroy($product)
+    {
+        return 'Respuesta desde controlador';
+    }
+
+}
